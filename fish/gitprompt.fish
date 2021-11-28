109e930df63c84c915f30cd3a94306c9ddc67384
Jur <jurjenm@yahoo.com>
2021-11-28 18:35:01 +0100

    Playing around with fish config

---
+++ fish/gitprompt.fish
@@ -0,0 +1,33 @@
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showupstream 'yes'
+
+set -l cl_empress '757575'
+set -l cl_gainsboro 'E0E0E0'
+set -l cl_dodger_blue '297EF2'
+set -l cl_gorse 'FFEB3B'
+set -l cl_red_orange 'F52D2D'
+set __fish_git_prompt_color_branch $cl_gorse -b $cl_empress
+set __fish_git_prompt_color_dirtystate $cl_dodger_blue -b $cl_gainsboro
+set __fish_git_prompt_color_invalidstate $cl_red_orange -b $cl_gainsboro
+set __fish_git_prompt_color_stagedstate $cl_dodger_blue -b $cl_gainsboro
+set __fish_git_prompt_color_cleanstate $cl_dodger_blue -b $cl_gainsboro
+set __fish_git_prompt_color_stashstate $cl_dodger_blue -b $cl_gainsboro
+set __fish_git_prompt_color_upstream $cl_gainsboro -b $cl_empress
+set __fish_git_prompt_color_untrackedfiles $cl_dodger_blue -b $cl_gainsboro
+set __fish_git_prompt_color $cl_gainsboro -b $cl_empress
+
+set __fish_git_prompt_char_cleanstate '  '
+set __fish_git_prompt_char_dirtystate '  '
+set __fish_git_prompt_char_invalidstate '  '
+set __fish_git_prompt_char_stagedstate '  '
+set __fish_git_prompt_char_stashstate '  '
+set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_untrackedfiles '  '
+set __fish_git_prompt_char_upstream_ahead '  '
+set __fish_git_prompt_char_upstream_behind '  '
+set __fish_git_prompt_char_upstream_diverged '  '
+set __fish_git_prompt_char_upstream_equal '  '
+set __fish_git_prompt_char_upstream_prefix ' '
