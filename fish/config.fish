3efb8c2529bab03309c0b4d28587c4d9301f43a6
Jur <jurjenm@yahoo.com>
2021-11-28 23:32:46 +0100

    streamlined path management (and my local installation)

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-#
-# Environment set up
-#
-
 set -x EDITOR vim
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
@@ -17,19 +13,23 @@ case '*'
     set -x OSTYPE "unknown"
 end
 
-for p in /usr/sbin /usr/local/sbin $HOME/go/bin $HOME/bin $HOME/bin/heroku/bin
-    contains $p $PATH
-    or set -x PATH $p $PATH
+function add_path
+    for p in $argv
+        contains $p $PATH
+        or set -x PATH $PATH $p
+    end
 end
+
+# take care to add node before heroku (heroku ships with old node binary)
+add_path /usr/sbin /usr/local/sbin $HOME/go/bin $HOME/bin
+add_path $HOME/bin/node-v16.13.0-linux-x64/bin/
+add_path $HOME/bin/firefox-download/
+add_path $HOME/bin/heroku/
 
 
 # SOURCE NONSTANDARD CONFIG FILES
 if [ -f $HOME/.config/fish/aliases.fish ]
     source $HOME/.config/fish/aliases.fish
-end
-
-if [ -f $HOME/.config/fish/private.fish ]
-    source $HOME/.config/fish/private.fish
 end
 
 if [ -f $HOME/config/fish/gitprompt.fish ]
