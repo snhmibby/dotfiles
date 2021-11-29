8a00417e3bf2fbce9f30b5139fcc2f0d75bbb16d
Jur <jurjenm@yahoo.com>
2021-11-29 14:01:44 +0100

    more fish config

--- fish/functions/fug.fish
+++ fish/functions/fug.fish
@@ -2,5 +2,7 @@ function fug --description "open vim and start fugitive, iff in a git dir"
 	git status &> /dev/null
 	if [ $status = 0 ]
 		vim -c G -c only
+	else
+		echo 'Not a git dir...'
 	end
 end
