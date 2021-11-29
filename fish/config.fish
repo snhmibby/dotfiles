a22ecffea80312e834914ce5830a1bc2797d26c8
Jur <jurjenm@yahoo.com>
2021-11-29 14:25:46 +0100

    add ~/.local/bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,10 @@ set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
 
 # take care to add node before heroku (heroku ships with old node binary)
-add_path /usr/sbin /usr/local/sbin $HOME/go/bin $HOME/bin
+add_path /usr/sbin /usr/local/sbin 
+add_path $HOME/.local/bin
+add_path $HOME/bin
+add_path $HOME/go/bin 
 add_path $HOME/bin/node-v16.13.0-linux-x64/bin/
 add_path $HOME/bin/firefox-download/
 add_path $HOME/bin/heroku/
