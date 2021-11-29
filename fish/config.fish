f507c72a615e8bd89d7e03be898e8658e3a0da78
Jur <jurjenm@yahoo.com>
2021-11-29 23:49:22 +0100

    back to conf.d

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,13 @@
 set -x EDITOR vim
+set -x BROWSER firefox
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
 
-# take care to add node before heroku (heroku ships with old node binary)
 add_path /usr/sbin /usr/local/sbin 
 add_path $HOME/.local/bin
 add_path $HOME/bin
 add_path $HOME/go/bin 
+# take care to add node before heroku (heroku ships with old node binary)
 add_path $HOME/bin/node-v16.13.0-linux-x64/bin/
 add_path $HOME/bin/firefox-download/
 add_path $HOME/bin/heroku/
-
-
-# source custom config
-for f in $HOME/.config/fish/custom/*
-    source $f
-end
