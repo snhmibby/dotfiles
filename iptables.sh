a65825935d85f9557b4f77723ce59d0a1ce6966d
jurjen <info@jurjen.dev>
2022-08-02 00:11:46 +0200

    now with ipv6

--- iptables.sh
+++ iptables.sh
@@ -1,12 +1,30 @@
+#!/bin/sh
 PATH='/sbin'
 #only allow outgoing connections
 
 #flush everything
 iptables -F
 
+# only allow outgoing
 iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 
 iptables -A INPUT -s 127.0.0.0/8 -d 127.0.0.0/8 -i lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+
+
+##########
+# IPv6
+# same as above
+ip6tables -F
+
+ip6tables -P INPUT DROP
+ip6tables -P FORWARD DROP
+ip6tables -P OUTPUT ACCEPT
+
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ip6tables -A INPUT -p icmpv6 -j ACCEPT
+exit 0
