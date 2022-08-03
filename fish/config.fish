1f21c5d4aa721b9f87c58cb7f4850cd6e02d058d
jurjen <info@jurjen.dev>
2022-08-03 15:39:01 +0200

    add user .local/bin directory in path

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,6 @@ set -x BROWSER firefox
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
 
-add_path /usr/sbin /usr/local/sbin 
+add_path /usr/sbin /usr/local/sbin $HOME/.local/bin
 
 starship init fish | source
