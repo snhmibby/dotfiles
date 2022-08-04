854abbe3a7168d54c919d69715c713dbedf0fb78
jurjen <info@jurjen.dev>
2022-08-04 11:51:11 +0200

    messing with conky

---
+++ conky/toggle.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# use xdotool to move a window on and off screen
+
+#########################################################
+# configuration variables
+
+# cannot move window entirely off screen, has to have 1 pixel on
+on_screenX=25
+on_screenY=25
+off_screenX=1919
+off_screenY=1079
+
+# how to get window with xdotool
+search="search --all --class conky" #--name 'laptop.jurjen.dev'"
+
+get_geometry() {
+	geom=$(xdotool $search getwindowgeometry --shell)
+	if [ $? -ne 0 ]; then
+		echo "Error: Cannot find window."
+		exit 1
+	fi
+	eval $geom
+}
+
+off_screen() {
+	test $X -ne $on_screenX
+}
+
+toggle_window() {
+	get_geometry
+	if off_screen; then
+		xdotool $search windowmove $on_screenX $on_screenY
+	else
+		xdotool $search windowmove $off_screenX $off_screenY
+	fi
+}
+
+toggle_window
