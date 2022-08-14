c7a0dee5171c13ca3018d87cdf3fd792be14f0ca
jurjen <info@jurjen.dev>
2022-08-14 22:49:41 +0200

    deleted old aliases

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -15,8 +15,3 @@ abbr ws sudo wireshark
 abbr m neomutt
 abbr mail neomutt
 abbr fm fetchmail
-
-# html documentation
-abbr doc-dev 'firefox devdocs.io'
-abbr doc-ghc 'firefox ~/.ghcup/share/doc/ghc-8.10.7/html/index.html'
-abbr doc-hugo 'cd ~/src/other/hugo/docs && hugo server -p 12345 &; sleep 2 && firefox localhost:12345'
