2716a517a21ac4ddf1c588d901eb2330b1401a6b
jurjen <info@jurjen.dev>
2022-12-24 23:01:41 +0100

    added sabaki path

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,12 @@ set -x BROWSER firefox
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
 
-add_path /usr/sbin /usr/local/sbin $HOME/.local/bin $HOME/.cabal/bin $HOME/.ghcup/bin
+# generic paths
+fish_add_path /usr/sbin /usr/local/sbin $HOME/.local/bin
+# haskell paths
+fish_add_path $HOME/.cabal/bin $HOME/.ghcup/bin
+# sabaki
+fish_add_path /opt/Sabaki
 
 starship init fish | source
 fish_vi_key_bindings
