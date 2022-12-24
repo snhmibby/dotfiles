086c80774ae978d618b0057edb0cbf6bbab7c2eb
jurjen <info@jurjen.dev>
2022-12-24 21:48:43 +0100

    oops typo

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set -x BROWSER firefox
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_greeting
 
-add_path /usr/sbin /usr/local/sbin $HOME/.local/bin $HOME/.cabal/bin $HOME/ghcup/bin
+add_path /usr/sbin /usr/local/sbin $HOME/.local/bin $HOME/.cabal/bin $HOME/.ghcup/bin
 
 starship init fish | source
 fish_vi_key_bindings
